28a1750ac66d515cab2c9855e72e82406b69f52e
Georgette <ggitonga3@usiu.ac.ke>
2025-04-17 16:01:35 +0300

    Added salary calculation system

---
+++ q1  IST 1025-endsem-673422-georgette.py
@@ -0,0 +1,43 @@
+#Python program to calculate employees' net salaries while consideritng taxation
+
+def calculate_salary(hourly_rate, hours_worked, tax_rate=0.15):
+    class hourly_rate({400}):
+     print( hourly_rate) == 400
+
+    class hours_worked ({50}):
+     print( hours_worked) == 50
+
+    #calculating gross salary
+    def gross_salary(hourly_rate, ** hours_worked):
+      class(total_gross_salary) = (20,000)
+       print(20,000)
+
+
+def tax_rate(int{0.15%})
+print ==15%
+    class tax_rate ({gross_salary**tax_rate})
+     (gross_salary*tax_rate)= ({15%*20,000})
+     print == 3,000
+
+def net_salary = (int{gross_salary - tax})
+    net_salary = (17,000)
+    print net_salary == 17,000
+
+
+def hourly_rate(Please enter your hourly rate):
+     if hourly_rate = 200
+     print = 200
+    else print = (int{300,400,500)
+                  
+def hours_worked (Please enter hours worked)
+   if hours_worked = 40
+    print = 40
+    else print =(int{50,60})
+
+    
+
+    
+
+
+
+
