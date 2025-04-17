ed44bdcfd4d7d24d342140b7d40ffc221406daa9
Georgette <ggitonga3@usiu.ac.ke>
2025-04-17 16:02:29 +0300

    Implemented banking system using OOP

---
+++ q2 IST 1025-endsem-673422-georgette.py
@@ -0,0 +1,18 @@
+class BankAccount:
+     __init__(self,account_number,balance=0)
+def[account_number]={123456789}
+    if deposit()= 5000
+    print = 5000
+    else: print = (3000, 4000, 6000)
+
+   withdrawal (2000)
+   print = 2000
+   else : print = (3000,4000,5000)
+
+   def check_balance
+      if balance = 0
+      print = 0
+      else : print = Invalid choice
+
+
+      
